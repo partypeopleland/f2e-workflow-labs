f2c8257f3c6dbe39fd49dc1f95bc42b2d0ba477b
art <partypeopleland@gmail.com>
2015-09-05 14:47:55 +0800

    設定 mytask1, mytask2 工作相依於 default 工作

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+gulp.task('default',["mytask1","mytask2"],function(){
+	console.log('My Default Task');
+})
+
+gulp.task('mytask1',function(){
+	console.log('Task1 Output');
+})
+
+gulp.task('mytask2',function(){
+	console.log('Task2 Output');
+})
