7c00e4fd0598de79941e8754ec204fd2dea842eb
art <partypeopleland@gmail.com>
2015-09-05 15:34:09 +0800

    將所有 assets/vendor/ 目錄下 (含子目錄) 所有 *.js 與 *.css 檔案全部輸出到 output3 資料夾下

--- gulpfile.js
+++ gulpfile.js
@@ -10,3 +10,21 @@ gulp.task('mytask1',["mytask2"],function(){
 gulp.task('mytask2',function(){
 	console.log('Task2 Output');
 })
+
+gulp.task('output1',["default"],function(){
+	gulp.src('asserts/vendor/bootstrap/**/*.js')
+	.pipe(gulp.dest('output1'));	
+})
+
+gulp.task('output2',["default"],function(){
+	gulp.src('asserts/vendor/bootstrap/**/*.js',{base:'asserts'})
+	.pipe(gulp.dest('output2'));	
+})
+
+gulp.task('output3',["default"],function(){
+	gulp.src([
+		'asserts/vendor/**/*.js',
+		'asserts/vendor/**/*.css'],{base:
+		'asserts/vendor'})
+		.pipe(gulp.dest('output3'));
+})
