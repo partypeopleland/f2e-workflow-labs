a9d2ec50a3eb57a78379a1df151be2307ff4dc37
art <partypeopleland@gmail.com>
2015-09-05 14:56:23 +0800

    gulp task call back練習

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,12 @@ gulp.task('default',["mytask1","mytask2"],function(){
 	console.log('My Default Task');
 })
 
-gulp.task('mytask1',function(){
+gulp.task('mytask1',function(cb){
 	console.log('Task1 Output');
+	cb();
 })
 
-gulp.task('mytask2',function(){
+gulp.task('mytask2',function(cb){
 	console.log('Task2 Output');
+	cb();
 })
