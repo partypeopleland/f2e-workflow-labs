2711386add81951d59b3f4732d237349af9bda99
art <partypeopleland@gmail.com>
2015-09-09 23:31:15 +0800

    at home practice

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,7 @@
 var gulp = require('gulp');
 var del = require('del');
 //透過gulp-load-plugins載入套件
-var $ = require('gulp-load-plugins')({
-	pattern: ['gulp-*', 'gulp.*'], // the glob(s) to search for 
-    config: 'package.json', // where to find the plugins, by default searched up from process.cwd() 
-    scope: ['dependencies', 'devDependencies', 'peerDependencies'], // which keys in the config to look within 
-    replaceString: /^gulp(-|\.)/, // what to remove from the name of the module when adding it to the context 
-    camelize: true, // if true, transforms hyphenated plugins names to camel case 
-    lazy: true, // whether the plugins should be lazy loaded on demand 
-    rename: {} // a mapping of plugins to rename 
-});
+var $ = require('gulp-load-plugins')();
 //var concat = require('gulp-concat');
 //var uglify = require('gulp-uglify');
 //var rename = require('gulp-rename');
